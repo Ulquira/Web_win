53dc8d2646aad80f9aa7c2ce840c7348daefc7ec
Ulquira <Tonnyabdo@gmail.com>
2026-05-25 12:22:51 -0500

    Remove dist from git tracking to fix language stats

--- dist/sw.js
+++
@@ -1,4 +0,0 @@
-// Este es un Service Worker vacío necesario para poder mostrar notificaciones push en dispositivos móviles.
-self.addEventListener('push', function(event) {
-  console.log('Push message received');
-});
