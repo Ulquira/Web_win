16e0ade0adc7a677e29633ab07d75a1a44781c43
Ulquira <tonnabdo@gmail.com>
2026-08-07 09:45:37 -0500

    style: apply Mi WIN brand guidelines (Colors and Made Tommy Typography)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,47 +1,60 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: ["class"],
+  darkMode: ['class'],
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./Index.tsx"
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './Index.tsx'
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Helvetica', 'Arial', 'sans-serif'],
-        corbel: ['Corbel', 'sans-serif'],
+        sans: ['Made Tommy', 'Helvetica', 'Arial', 'sans-serif'],
       },
       colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
         primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-          light: "hsl(var(--primary-light))",
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
         },
         secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
         },
         destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        success: {
+          DEFAULT: '#16a34a',
+          bg: '#dcfce8',
+        },
+        warning: {
+          DEFAULT: '#caa204',
+          bg: '#fef2c3',
+        },
+        error: {
+          DEFAULT: '#e02424',
+          bg: '#fef2f2',
+        },
+        win: {
+          orange: '#FF5A0A',
+          black: '#0F090B',
+          yellow: '#ffc200',
+          gray: '#f2f2f2',
+          grayDark: '#e8e7e8',
         },
         muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
         },
         card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
         },
       },
     },
